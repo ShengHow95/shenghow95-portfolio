88711a55f10cb4ea3572b5b2bf135973e1d0ff90
Jeff Kong <jeffkong>
2026-06-24 22:01:56 +0800

    fix: update nuxt.config.ts to include NUXT_APP_BASE_URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   app: {
+    baseURL: process.env.NUXT_APP_BASE_URL || '/',
     head: {
       title: 'Sheng How Kong | Cloud/Backend Engineer',
       meta: [
